7d1ac45e3aa6e7a039586a273cca0037177fa89e
Asemco <crasykid2@hotmail.com>
2020-03-27 12:37:27 -0400

    - Found out why it's not working on Deploy. - Gave it a... fitting package name.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyAngularMaterialModule } from "../assets/MyAngularMaterialModule";
